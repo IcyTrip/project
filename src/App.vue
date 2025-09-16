b5a165ae96283346f1dbb0f93c5d2703025f3282
IcyTrip <whittinghamkian@gmail.com>
2025-09-16 14:28:54 -0600

    w

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
 import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
   <header>
-    <Navbar />
+
   </header>
   <main>
     <RouterView/>
